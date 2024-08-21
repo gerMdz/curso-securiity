753446383a58cc69845e9772163fca287b59f3e5
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2024-08-21 08:58:27 -0300

    Add CSRF protection to login form
    Implement CSRF token validation in LoginFormAuthenticator and include CSRF token field in the login form template. Update security configuration to support CSRF protection and add a remember_me section.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
@@ -28,7 +29,7 @@ class LoginFormAuthenticator extends AbstractAuthenticator
     {
     }
 
-    public function supports(Request $request): ?bool
+    public function supports(Request $request): bool|null
     {
         return $request->getPathInfo() === '/login' && $request->isMethod('POST');
     }
@@ -51,7 +52,13 @@ class LoginFormAuthenticator extends AbstractAuthenticator
 
                     return $user;
                 }),
-            new PasswordCredentials($password)
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge(
+                    'authenticate',
+                    $request->request->get('_csrf_token')
+                )
+            ]
         );
     }
 
